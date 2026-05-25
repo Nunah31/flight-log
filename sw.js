0f83839d03f9ca41d0f90f5acff7c0e3bd0575e7
Nunah31 <estherhabari@gmail.com>
2026-05-25 14:15:38 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', () => self.skipWaiting())
-self.addEventListener('activate', () => self.clients.claim())
-self.addEventListener('fetch', e => {
-  if (e.request.url.includes('api.anthropic.com') || e.request.url.includes('googleapis.com') || e.request.url.includes('cdnjs')) return;
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
-})
